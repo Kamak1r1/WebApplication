860680903544726485fff9819290ff981744361c
Aleksey Suchkov <ashgrey1011@gmail.com>
2021-12-26 09:40:39 +0300

    Add project files.

---
+++ WebApplication/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using WebApplication.Areas.Identity.Data;
+using WebApplication.Data;
+using Npgsql.EntityFrameworkCore;
+
+[assembly: HostingStartup(typeof(WebApplication.Areas.Identity.IdentityHostingStartup))]
+namespace WebApplication.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<AuthDbContext>(options =>
+                    options.UseNpgsql(
+                        context.Configuration.GetConnectionString("AuthDbContextConnection")));
+
+                services.AddDefaultIdentity<ApplicationUser>(options => {
+                    options.Password.RequireLowercase = false;
+                    options.Password.RequireUppercase = false;
+                    options.SignIn.RequireConfirmedAccount = false;
+                })
+                .AddEntityFrameworkStores<AuthDbContext>();
+            });
+        }
+    }
+}
